1b9849bf976fbdb60f9c576dea93b9fd377fcc20
vijay4211 <vijayppandit2042.com>
2024-06-13 18:14:47 +0530

    practice of arraylist, likedlist and vector code

--- Myclass.java
+++ Myclass.java
@@ -1,61 +1,18 @@
-      
+import java.util.*;
 
-
-public class myclass{
-    public static void main(String[] args){
-        //==========Ex-1
-        int x,y,z;
-        x=10;
-        y=2;
-        z = x/y;
-        System.out.println(z);
-        //==========Output
-        //5
-
-        //=======Ex-2
-        int x1, y1, z1;
-        x1=10;
-        y1=0;
-        z1 = x1/y1;
-        System.out.println(z1);
-        //=========Output
-        //java.lang.ArithmeticException
-
-        //==========Ex-3
-        int x2, y2, z2;
-        x2=10;
-        y2=0;
-        z2=x2/y2;
-        //show error
-        System.out.println("z2" + z2);
-        //Remaining code not show error
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        //=========Output
-        //java.lang.ArithmeticException
-
-
-        //==========Ex-3
-        int x3, y3, z3;
-        x3=10;
-        y3=0;
-        z3=x3/y3;
-        //show error
-        System.out.println("z3" + z3);
-        //Remaining code not show error
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-        System.out.println("Welcome");
-
-
-
-
-
-
-
-
+public class myclass {
+    @SuppressWarnings("unchecked")
+    public static void main(String[] args) {
+        @SuppressWarnings("rawtypes")
+        ArrayList arrlist = new ArrayList();
+        arrlist.add(10); // index = 0
+        arrlist.add(20); // index = 1
+        arrlist.add("mango"); // index = 2
+        arrlist.add(10.6); // index = 3
+        System.out.println(arrlist);
+        System.out.println(arrlist.size());
     }
 }
+// ========Output
+// [10, 20, mango, 10.6]
+// 4
